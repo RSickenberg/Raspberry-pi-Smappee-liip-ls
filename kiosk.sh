7986dd39d50767489c6c15ae42ea3225c007e05d
Smapee-pi-ls <pi@ls.liip.ch>
2019-11-06 11:49:31 +0100

    Added service

--- kiosk.sh
+++ kiosk.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # Made by Romain Sickenberg with 💚🌍
+# Source: https://pimylifeup.com/raspberry-pi-kiosk/
 
 # Disable display power management system from kicking in and blanking out the screen.
 # Basically, these three commands set the current xsession not to blank out the screensaver and then disables the screensaver altogether. The third line disables the entire “display power management system” meaning that the desktop interface should never blank out the screen.
@@ -14,3 +15,6 @@ unclutter -idle 0.5 -root &
 # Sed used to search inside Chromium preferences to toggle some config.
 sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' /home/pi/.config/chromium/Default/Preferences
 sed -i 's/"exit_type":"Crashed"/"exit_type":"Normal"/' /home/pi/.config/chromium/Default/Preferences
+
+# Launch Chromium
+/usr/bin/chromium-browser --noerrdialogs --disable-infobars --kiosk https://my.smappee.com/#home https://my.smappee.com/#consumption &
